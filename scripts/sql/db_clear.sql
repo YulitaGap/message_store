10e567ed48c5392b2bb0c6147083ea206c24f705
Артемій <44300059+Katerunner@users.noreply.github.com>
2020-05-28 20:00:45 +0300

    Update db_clear.sql

--- scripts/sql/db_clear.sql
+++ scripts/sql/db_clear.sql
@@ -1,4 +1,4 @@
 -- clear from all content
 
 TRUNCATE orders, posts, discount, account, style, author_agent,
-agent, author, principal, social_network RESTART IDENTITY CASCADE;
+agent, author, principal, social_network, authentication RESTART IDENTITY CASCADE;
