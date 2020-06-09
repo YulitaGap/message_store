9908d07131550d85f811953bbc7c6aa7b9b50b56
fenix-125 <yurijpasichnyk11@gmail.com>
2020-06-09 15:29:02 +0300

    delete unused endpoints from site

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template
 from flask_restful import Api
 
 from modules.api_endpoints import *
@@ -30,73 +30,6 @@ def register_get():
     render the register page
     """
     return render_template("main/register.html")
-
-
-# #########################################################################
-
-
-@app.route("/register_demo", methods=["POST"])
-def register():
-    """
-    Try to register the user
-    """
-    user = request.form.get("login")
-    pasw = request.form.get("pasw")
-
-    if not user or not pasw:
-        return redirect(url_for("fail", _method="GET"))
-
-    email = request.form.get("email")
-    name = request.form.get("name")
-    phone = request.form.get("phone")
-    company = request.form.get("company")
-    query = {
-        "login": user,
-        "id": auth.get_new_id(),
-        "type": "moder",
-        "full_name": name,
-        "mob_numb": phone,
-        "email": email,
-        "company": company,
-        "pasw": pasw,
-        "access": []
-    }
-    if auth.register(query):
-        return redirect(url_for("login", _method="GET"))
-    else:
-        return redirect(url_for("fail", _method="GET"))
-
-
-@app.route('/check_username', methods=["POST"])
-def check_username():
-    """
-    Secondary function to check if the login if available
-    :return: bool
-    """
-    username = request.args.get("username")
-    return '1' if auth.is_login_free(username) else '0'
-
-
-@app.route('/auten', methods=["POST", "GET"])
-def auten():
-    """
-    Authenticate the user login
-    """
-    user = request.args.get("login")
-    pasw = request.args.get("pasw")
-    print()
-    print(user, pasw)
-    print()
-    if user and pasw and auth.verify(user, pasw):
-        # "Success!"
-        permission = auth.user_permission(user, pasw)
-        print(permission)
-        if permission != "admin":
-            return redirect(url_for("form_submit", user=user, pasw=pasw))
-        else:
-            return redirect(url_for("admin", user=user, pasw=pasw))
-    else:
-        return redirect(url_for("login", fail=True, _method="GET"))
 
 
 @app.route('/fail', methods=['GET'])
